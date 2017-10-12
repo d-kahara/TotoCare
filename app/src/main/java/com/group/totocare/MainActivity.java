c3a06e461897bc3d54e308b63051f185c37ab7f9
rahmakevo <siundu344@gmail.com>
2017-10-12 14:56:59 +0300

    creating onOptions menu in java MainActivity

--- app/src/main/java/com/group/totocare/MainActivity.java
+++ app/src/main/java/com/group/totocare/MainActivity.java
@@ -2,6 +2,9 @@ package com.group.totocare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*
+    onOptionsMenu for Main Activity
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*
+        instantiate a Menu Inflater
+         */
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_mainactivity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
