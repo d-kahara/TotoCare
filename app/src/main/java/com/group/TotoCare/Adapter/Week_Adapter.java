7789cd1f5b43ff481aa4a6ffa8a8124b16749111
rahmakevo <siundu344@gmail.com>
2017-10-24 10:34:07 +0300

    working on baby week data

---
+++ app/src/main/java/com/group/TotoCare/Adapter/Week_Adapter.java
@@ -0,0 +1,8 @@
+package com.group.TotoCare.Adapter;
+
+/**
+ * Created by rahmak on 10/23/17.
+ */
+
+public class Week_Adapter {
+}
---
+++ app/src/main/java/com/group/TotoCare/model/Weeks.java
@@ -0,0 +1,8 @@
+package com.group.TotoCare.model;
+
+/**
+ * Created by rahmak on 10/23/17.
+ */
+
+public class Weeks {
+}
