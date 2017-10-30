351bdd412044d190e0dd9959f2635112646e2ea8
David Kahara <kaharadave@gmail.com>
2017-10-30 08:25:31 +0300

    Data retrieval from firebase onto this fragment

--- app/src/main/java/com/group/TotoCare/Fragments/BabyFragment.java
+++ app/src/main/java/com/group/TotoCare/Fragments/BabyFragment.java
@@ -2,6 +2,9 @@ package com.group.TotoCare.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,57 +16,56 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.group.TotoCare.Adapter.FirebaseFoodListAdapter;
+import com.group.TotoCare.Adapter.FirebaseFoodViewHolder;
 import com.group.TotoCare.R;
 import com.group.TotoCare.model.Food;
 
 import java.util.ArrayList;
 
-public class BabyFragment extends Fragment{
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private DatabaseReference mDatabase;
-    private ArrayList<Food> foodList=new ArrayList<>();
-    private ListView mFoodsListView;
-    private TextView mFoodView;
-    private TextView mNutrientView;
+public class BabyFragment extends Fragment {
+    private FirebaseFoodListAdapter mFirebaseAdapter;
 
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
 
+    // Required empty public constructor
+    public BabyFragment() {}
 
-
-    //Requires an empty constructor
-    public BabyFragment(){
-
-    }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
-//call the layout for the fragment
-        View myView=inflater.inflate(R.layout.activity_baby_fragment, container, false);
-
-        mFoodsListView=(ListView) myView.findViewById(R.id.foodsListView);
-
-        mDatabase = FirebaseDatabase.getInstance().getReference("nutrition");
-//        mNutrientView=(TextView) getActivity().findViewById(R.id.nutrientView);
-
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot data:dataSnapshot.getChildren()){
-                    foodList.add(data.getValue(Food.class));
-                }
-
-                ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,foodList);
-                mFoodsListView.setAdapter(adapter);
-            }
-
-            //method fired up in the event of any error
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        return myView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_baby_fragment, container, false);
+        ButterKnife.bind(this, view);
+        setUpFirebaseAdapter();
+        return view;
     }
+
+    private void setUpFirebaseAdapter() {
+        Query query = FirebaseDatabase.getInstance()
+                .getReference("nutrition");
+
+        mFirebaseAdapter = new FirebaseFoodListAdapter(Food.class, R.layout.food_list_item, FirebaseFoodViewHolder.class,
+                query, getActivity());
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+
+    }
+
+    @Override
+    //method is now public
+    public void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
 
 }
